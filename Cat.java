bf5ec81f0b42c988db70c05135f4df41d2bd5d63
Justin NMJ <nmjnshokano@gmail.com>
2025-03-31 18:59:34 +0300

    Delete Cat.java

--- Cat.java
+++
@@ -1,8 +0,0 @@
-
-    // Cat.java - Subclass overriding
-    public class Cat extends Animal {
-        @Override
-        public void makeSound() {
-            System.out.println("Cat meows: Meow! Meow!");
-        }
-    }
