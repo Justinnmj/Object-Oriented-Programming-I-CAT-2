a1ececbb9e82b7d5e68cbeaa0935aad0451b239b
Justinnmj <nmjnshokano@gmail.com>
2025-03-31 18:20:39 +0300

    good

---
+++ Cat.java
@@ -0,0 +1,8 @@
+
+    // Cat.java - Subclass overriding
+    public class Cat extends Animal {
+        @Override
+        public void makeSound() {
+            System.out.println("Cat meows: Meow! Meow!");
+        }
+    }
